960e834d659670adda50f1698cbc6b50092f950b
Vaishnavi0717 <vaishnavipunekar0@.com>
2023-12-07 15:34:03 +0530

    nsmsms

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const { connection } = require("./db");
 const { userRouter } = require("./routes/post.routes");
 const cors = require("cors")
 const app = express();
-const port=8080;
+require('dotenv').config()
 
 app.use(express.json());
 app.use(cors());
 
 app.use("/contacts" , userRouter)
 
-app.listen(port , async()=>{
+app.listen(process.env.PORT, async()=>{
     try {
         await connection;
         console.log("server is running at port 8080")
